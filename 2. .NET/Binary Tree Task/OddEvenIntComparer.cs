190c15f098a3d7ad804fe6d78072b250dd018d17
Artem Serediuk <a.serediuk@student.csn.khai.edu>
2022-08-09 20:32:39 +0300

    Feature/binary tree

---
+++ 2. .NET/Binary Tree Task/OddEvenIntComparer.cs
@@ -0,0 +1,20 @@
+﻿namespace Binary_Tree_Task
+{
+    public class OddEvenIntComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 == 0 || x % 2 != 0 && y % 2 != 0)
+            {
+                return x.CompareTo(y);
+            }
+
+            if (x % 2 == 0)
+            {
+                return 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ 2. .NET/Binary Tree Task/ToBinaryTreeExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace Binary_Tree_Task
+{
+    public static class ToBinaryTreeExtension
+    {
+        public static BinaryTree<T> ToBinaryTree<T>(this IEnumerable<T> enumerable) where T : IComparable<T>
+        {
+            var tree = new BinaryTree<T>();
+            tree.AddRange(enumerable);
+            return tree;
+        }
+
+        public static BinaryTree<T> ToBinaryTree<T>(this IEnumerable<T> enumerable, IComparer<T> comparer, IEnumerator<T> enumerator)
+            where T : IComparable<T>
+        {
+            var tree = new BinaryTree<T>() { Comparer = comparer, Enumerator = enumerator };
+            tree.AddRange(enumerable);
+            return tree;
+        }
+    }
+}
